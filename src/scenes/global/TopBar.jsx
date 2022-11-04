ff69281677f435fb0bcf90087f42288563872ca7
Ray Guo <ma2808203259@hotmail.com>
2022-11-05 01:29:03 +1100

    init project

---
+++ src/scenes/global/TopBar.jsx
@@ -0,0 +1,10 @@
+import { Box, IconButton, useTheme } from '@mui/material';
+import { useContext } from 'react';
+import { ColorModeContext, tokens } from '../../theme';
+import InputBase from '@mui/material';
+import { LightModeOutlined } from '@mui/icons-material';
+
+const TopBar = () => {
+  return <div>TopBar</div>;
+};
+export default TopBar;
